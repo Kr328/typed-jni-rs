aacdc28a574deaa144abf698f590c0caee23e9b8
Kr328 <kr328app@outlook.com>
2025-10-21 19:37:03 +0800

    Fix: fix cache with example and tests

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,9 +1,9 @@
 use std::{path::Path, process::Stdio};
 
 use example::JavaExample;
-use jni::JavaVM;
 use typed_jni::{
-    DynArg, LocalClass, LocalObject, Type, TypedCallExt, TypedClassExt, TypedFieldAccessExt, TypedStringExt, define_java_class,
+    DynArg, LocalClass, LocalObject, Type, TypedCallExt, TypedClassExt, TypedFieldAccessExt, TypedStringExt, core::JavaVM,
+    define_java_class,
 };
 
 define_java_class!(JavaSystem, "java.lang.System");
@@ -33,7 +33,7 @@ fn main() {
 
     let library_path = std::env::current_exe().unwrap();
 
-    let vm = JavaVM::new(
+    let vm = jni::JavaVM::new(
         jni::InitArgsBuilder::new()
             .option(format!(
                 "-Djava.library.path={}",
@@ -45,23 +45,24 @@ fn main() {
     )
     .unwrap();
 
-    let env = vm.attach_current_thread().unwrap();
+    let vm = unsafe { JavaVM::from_raw(vm.get_java_vm_pointer() as _) };
 
-    let env = unsafe { typed_jni::core::JNIEnv::from_raw(env.get_raw() as _) };
+    vm.with_attached_thread(false, |env| {
+        let c_system: LocalClass<JavaSystem> = env.typed_find_class().unwrap();
+        let o_out: LocalObject<JavaPrintStream> = env.typed_get_field(&c_system, "out").unwrap();
 
-    let c_system: LocalClass<JavaSystem> = env.typed_find_class().unwrap();
-    let o_out: LocalObject<JavaPrintStream> = env.typed_get_field(&c_system, "out").unwrap();
+        env.typed_call_method::<(), _, _>(&o_out, "println", &[&env.typed_new_string("Hello World!") as &dyn DynArg])
+            .unwrap();
 
-    env.typed_call_method::<(), _, _>(&o_out, "println", &[&env.typed_new_string("Hello World!") as &dyn DynArg])
-        .unwrap();
+        env.typed_call_method::<(), _, _>(&o_out, "println", (env.typed_new_string("Hello World!!!!"),))
+            .unwrap();
 
-    env.typed_call_method::<(), _, _>(&o_out, "println", (env.typed_new_string("Hello World!!!!"),))
-        .unwrap();
+        env.typed_call_method::<(), _, _>(&o_out, "println", (env.typed_new_string("Hello World!!!!!!!!!"),))
+            .unwrap();
 
-    env.typed_call_method::<(), _, _>(&o_out, "println", (env.typed_new_string("Hello World!!!!!!!!!"),))
-        .unwrap();
+        let v: LocalClass<JavaExample> = env.typed_find_class().unwrap();
 
-    let v: LocalClass<JavaExample> = env.typed_find_class().unwrap();
-
-    let _: () = env.typed_call_method(&v, "run", ()).unwrap();
+        let _: () = env.typed_call_method(&v, "run", ()).unwrap();
+    })
+    .unwrap();
 }
